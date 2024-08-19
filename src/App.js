c5344a4bd1f9bd9fafe55d1afbe35319061d631f
Euclides Silva <euclides.silva@accurate.com.br>
2024-08-19 00:22:51 -0300

    atualização, para pegar o audio da aba

--- src/App.js
+++ src/App.js
@@ -11,8 +11,16 @@ const AudioRecorder = () => {
   const [transcript, setTranscript] = useState('');
   const [audioURL, setAudioURL] = useState('');
   const [loadingDOCX, setLoadingDOCX] = useState(false);
+
+  const [recordingTab, setRecordingTab] = useState(false);
+  const [transcriptTab, setTranscriptTab] = useState('');
+  const [audioURLTab, setAudioURLTab] = useState('');
+  const [loadingDOCXTab, setLoadingDOCXTab] = useState(false);
+
   const mediaRecorderRef = useRef(null);
+  const mediaRecorderTabRef = useRef(null);
   const recognitionRef = useRef(null);
+  const recognitionTabRef = useRef(null);
 
   const startRecording = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -62,7 +70,55 @@ const AudioRecorder = () => {
     setRecording(false);
   };
 
-  const generateDOCX = async () => {
+  const startRecordingTab = async () => {
+    const stream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
+    const mediaRecorderTab = new MediaRecorder(stream);
+    const audioChunksTab = [];
+
+    mediaRecorderTab.ondataavailable = event => {
+      audioChunksTab.push(event.data);
+    };
+
+    mediaRecorderTab.onstop = () => {
+      const audioBlobTab = new Blob(audioChunksTab, { type: 'audio/wav' });
+      const audioUrlTab = URL.createObjectURL(audioBlobTab);
+      setAudioURLTab(audioUrlTab);
+    };
+
+    mediaRecorderTabRef.current = mediaRecorderTab;
+    mediaRecorderTab.start();
+
+    const recognitionTab = new window.webkitSpeechRecognition();
+    recognitionTab.lang = 'pt-BR';
+    recognitionTab.continuous = true;
+    recognitionTab.interimResults = true;
+
+    recognitionTab.onresult = event => {
+      const interimTranscriptTab = Array.from(event.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+
+      setTranscriptTab(interimTranscriptTab);
+    };
+
+    recognitionTab.onerror = event => {
+      console.error('Erro na transcrição do áudio da aba:', event.error);
+    };
+
+    recognitionTab.start();
+    recognitionTabRef.current = recognitionTab;
+
+    setRecordingTab(true);
+  };
+
+  const stopRecordingTab = () => {
+    mediaRecorderTabRef.current.stop();
+    recognitionTabRef.current.stop();
+    setRecordingTab(false);
+  };
+
+  const generateDOCX = async (text, filename) => {
     setLoadingDOCX(true);
     const doc = new Document({
       sections: [
@@ -76,7 +132,7 @@ const AudioRecorder = () => {
             new Paragraph({
               children: [
                 new TextRun({
-                  text: transcript,
+                  text,
                   font: 'Arial',
                 }),
               ],
@@ -86,19 +142,16 @@ const AudioRecorder = () => {
       ],
     });
 
-    // Salvar o arquivo DOCX
     const blob = await Packer.toBlob(doc);
     const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.href = url;
-    link.download = 'transcription.docx';
+    link.download = `${filename}.docx`;
     link.click();
     URL.revokeObjectURL(url);
 
     setLoadingDOCX(false);
   };
-
-  useEffect(() => console.log("Transcrição: ", transcript), [transcript]);
 
   return (
     <Container>
@@ -113,18 +166,39 @@ const AudioRecorder = () => {
           onClick={recording ? stopRecording : startRecording}
           sx={{ fontSize: 18, padding: '8px 20px', marginBottom: '10px' }}
         >
-          {recording ? 'Parar Gravação' : 'Iniciar Gravação'}
+          {recording ? 'Parar Gravação Microfone' : 'Iniciar Gravação Microfone'}
+        </Button>
+
+        <Button
+          variant="contained"
+          color={recordingTab ? 'error' : 'success'}
+          startIcon={recordingTab ? <StopIcon /> : <PlayArrowIcon />}
+          onClick={recordingTab ? stopRecordingTab : startRecordingTab}
+          sx={{ fontSize: 18, padding: '8px 20px', marginBottom: '10px' }}
+        >
+          {recordingTab ? 'Parar Gravação da Aba' : 'Iniciar Gravação da Aula'}
         </Button>
 
         <Button
           variant="contained"
           color="primary"
           startIcon={loadingDOCX ? <CircularProgress size={24} /> : <PictureAsPdfIcon />}
-          onClick={generateDOCX}
+          onClick={() => generateDOCX(transcript, 'transcription_microphone')}
           sx={{ fontSize: 18, padding: '8px 20px', marginBottom: '10px' }}
           disabled={!transcript || loadingDOCX}
         >
-          {loadingDOCX ? 'Gerando DOCX...' : 'Gerar DOCX'}
+          {loadingDOCX ? 'Gerando DOCX do Microfone' : 'Gerar DOCX do Microfone'}
+        </Button>
+
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={loadingDOCXTab ? <CircularProgress size={24} /> : <PictureAsPdfIcon />}
+          onClick={() => generateDOCX(transcriptTab, 'transcription_tab')}
+          sx={{ fontSize: 18, padding: '8px 20px', marginBottom: '10px' }}
+          disabled={!transcriptTab || loadingDOCXTab}
+        >
+          {loadingDOCXTab ? 'Gerando DOCX da Aula' : 'Gerar DOCX da Aula'}
         </Button>
 
         {audioURL && (
@@ -132,11 +206,24 @@ const AudioRecorder = () => {
             variant="contained"
             color="secondary"
             startIcon={<AudiotrackIcon />}
-            sx={{ fontSize: 18, padding: '8px 20px' }}
+            sx={{ fontSize: 18, padding: '8px 20px', marginTop: '10px' }}
             href={audioURL}
-            download="audio.wav"
+            download="audio_microphone.wav"
           >
-            Baixar Áudio
+            Baixar Áudio do Microfone
+          </Button>
+        )}
+
+        {audioURLTab && (
+          <Button
+            variant="contained"
+            color="secondary"
+            startIcon={<AudiotrackIcon />}
+            sx={{ fontSize: 18, padding: '8px 20px', marginTop: '10px' }}
+            href={audioURLTab}
+            download="audio_tab.wav"
+          >
+            Baixar Áudio da Aula
           </Button>
         )}
       </div>
